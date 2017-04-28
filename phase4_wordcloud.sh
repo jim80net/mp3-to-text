5598289261882b135acd61410057be776acdd263
Jim Park <jpark@jim80.net>
2017-04-27 23:39:04 -0700

    make a wordcloud!

---
+++ phase4_wordcloud.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./text_to_wordcloud_fodder.sh | wordcloud_cli.py > wordcloud.png
--- process.sh
+++ process.sh
@@ -3,3 +3,4 @@
 ./phase1_convert_all_to_flac.sh
 ./phase2_upload_to_watson.sh
 ./phase3_just_the_text.sh
+./phase4_wordcloud.sh
