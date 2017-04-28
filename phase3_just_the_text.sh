2c22a8ea7f58e661615ed21b1c8bfa8dce3ea294
Jim Park <jpark@jim80.net>
2017-04-27 22:57:39 -0700

    phase3 complete!

---
+++ phase3_just_the_text.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in $(ls *.json); do
+  NAME=$(echo $i| sed -e 's/\.json//')
+  if [[ -f ${NAME}.txt ]]; then
+    echo $NAME.txt already exists >&2
+  else
+    echo creating $NAME.txt >&2
+    cat $NAME.json | ./just_the_text.sh > $NAME.txt
+  fi
+done
--- process.sh
+++ process.sh
@@ -2,3 +2,4 @@
 
 ./phase1_convert_all_to_flac.sh
 ./phase2_upload_to_watson.sh
+./phase3_just_the_text.sh
