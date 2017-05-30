961d2e631801f473f2fab79888b6127684820531
Jim Park <jpark@jim80.net>
2017-05-29 18:59:36 -0700

    continuous is no longer supported; and i want curse words, damn it

--- upload_to_watson.sh
+++ upload_to_watson.sh
@@ -4,4 +4,4 @@ curl -X POST -u c3478cba-4ee4-473d-b7f7-ec36a51da5d4:${PASSWORD:?} \
 --header "Content-Type: audio/flac" \
 --header "Transfer-Encoding: chunked" \
 --data-binary @${1:?} \
-"https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"
+"https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?profanity_filter=false&smart_formatting=true"
